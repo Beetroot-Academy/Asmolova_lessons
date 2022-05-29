6f056c146ac0c495df3def51fd3f6eb035dd7d09
Dariia-Asmolova <dashaasmolova06@gmail.com>
2022-05-29 10:28:05 +0300

    completed hw_17

---
+++ hw_17/scripts/main.js
@@ -0,0 +1,15 @@
+// let userName = prompt ('What is your name?');
+// console.log(userName);
+// alert(`Hello ${userName}!`);
+
+// const currentYear = 2022;
+// let userBirthday = prompt ('When`s your birhday?');
+// console.log (currentYear - userBirthday)
+
+// let lengthSideOfSquad = prompt("Input length side of squad: ");
+// let perimetr = lengthSideOfSquad * 4;
+// alert(`Squad perimetr: ${perimetr}`);
+
+let circleRadius = prompt("Input circle radius: ");
+let circleSquare = Math.PI * circleRadius ** 2;
+alert(`Circle square: ${~~circleSquare}`);
