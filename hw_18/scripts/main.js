539ff40519424a0b3f13ee8cda6a9283b36c019d
Dariia-Asmolova <dashaasmolova06@gmail.com>
2022-06-03 20:06:01 +0300

    completed hw_18

---
+++ hw_18/scripts/main.js
@@ -0,0 +1,37 @@
+// min
+
+// 1
+// let x;
+// console.log((0.1 + 0.2).toFixed(1));
+
+// 2
+// let a = `1`;
+// let b = 2;
+// console.log(+a + b);
+
+// 3
+// let memory = prompt(`What size your flash drive?`);
+// alert(`You can download ${Math.floor(memory * 1024 / 820)} files`);
+
+// norm
+
+// 1
+// let yourMoney = +prompt('How much money do you have?');
+// let chocolatePrice = +prompt('What`s the price of the chocolate?');
+// let chocolateSum = Math.floor(yourMoney/chocolatePrice);
+// alert(`You can buy ${chocolateSum} chocolate and your rest will be ${yourMoney - chocolateSum * chocolatePrice} uah`);
+
+// 2
+// let yourNumber = prompt('Enter a three-digit number');
+// let firstNumber = yourNumber % 10;
+// let secondNumber = (yourNumber - firstNumber) / 10 % 10;
+// let thirdNumber = (yourNumber - firstNumber - secondNumber) / 100 % 10;
+// let reverseNumber = firstNumber * 100 + secondNumber * 10 + thirdNumber;
+// alert(`${reverseNumber.toFixed(0)}`);
+
+// max
+
+// 2
+// 2 && 0 && 3; // 0
+// 2 || 0 || 3; // 2
+// 2 && 0 || 3; // 3
