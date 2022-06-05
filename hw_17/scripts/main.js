404f7cb12cbf49381c1fff59f7f3e8a959d6cb51
Dariia-Asmolova <dashaasmolova06@gmail.com>
2022-06-05 08:46:50 +0300

    change

--- hw_17/scripts/main.js
+++ hw_17/scripts/main.js
@@ -1,13 +1,13 @@
-// let userName = prompt ('What is your name?');
+// let userName = prompt('What is your name?');
 // console.log(userName);
 // alert(`Hello ${userName}!`);
 
 // const currentYear = 2022;
-// let userBirthday = +prompt ('When`s your birhday?');
+// let userBirthday = +prompt('When`s your birhday?');
 // console.log (currentYear - userBirthday)
 
-// let lengthSideOfSquad = +prompt("Input length side of squad: ");
-// let perimetr = lengthSideOfSquad * 4;
+// let squadSideLength = +prompt("Input length side of squad: ");
+// let perimetr = squadSideLength * 4;
 // alert(`Squad perimetr: ${perimetr}`);
 
 let circleRadius = +prompt("Input circle radius: ");
